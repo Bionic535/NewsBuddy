2018c9db8c1b6c5ffbe887378f0116144b9c74e6
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-07-22 22:03:56 +0800

    base electron project set up

---
+++ src/electron/main.ts
@@ -0,0 +1,13 @@
+import {} from "electron";
+import { app, BrowserWindow } from "electron";  
+import * as path from "path";
+import { isDev } from "./util.js";
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({});
+    if (isDev()) {
+        mainWindow.loadURL("http://localhost:5123");
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+    }
+});
