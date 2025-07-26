d5d4f64162f3617fc78690d6ba19aed470f869f9
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-07-26 22:48:00 +0800

    initial input form started

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -7,6 +7,7 @@ app.on("ready", () => {
     const mainWindow = new BrowserWindow({});
     if (isDev()) {
         mainWindow.loadURL("http://localhost:5123");
+        mainWindow.webContents.openDevTools();
     } else {
         mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
     }
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,28 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1 className="text-4xl font-bold">NewsBuddy</h1>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form>
+          <input type="text" placeholder="Article Link Here" />
+          <div>
+            <button type="submit">Go</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Ctrl + S for summary | Ctrl + C to Check the Article
       </p>
     </>
   )
