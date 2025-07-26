55c46360916371d3cf9052fea2fd916b281dfdb9
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-07-26 23:12:27 +0800

    split form into 2nd file

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,17 +1,12 @@
 import './App.css'
-
+import MyForm from './form'
 function App() {
 
   return (
     <>
       <h1 className="text-4xl font-bold">NewsBuddy</h1>
       <div>
-        <form>
-          <input type="text" placeholder="Article Link Here" />
-          <div>
-            <button type="submit">Go</button>
-          </div>
-        </form>
+        <MyForm />
       </div>
       <p>
         Ctrl + S for summary | Ctrl + C to Check the Article
---
+++ src/ui/form.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function MyForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Article Link Here" />
+      <div>
+        <button type="submit">Go</button>
+      </div>
+    </form>
+  )
+}
+
+export default MyForm
