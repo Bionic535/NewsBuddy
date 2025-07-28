553227805f3a4fa54689aef0699dbcadc88c46cb
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-07-28 20:23:25 +0800

    form submit logic working

--- src/ui/form.tsx
+++ src/ui/form.tsx
@@ -1,11 +1,29 @@
 import "./App.css"
+import { useState } from "react"
+
 
 function MyForm() {
+  const [articleLink, setArticleLink] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Article Link Submitted:", articleLink);
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Article Link Here" />
+    <form onSubmit={handleSubmit}>
       <div>
-        <button type="submit">Go</button>
+        <label>Enter the article link:
+          <input
+            className = "border"
+            type="text"
+            value={articleLink}
+            onChange={(event) => setArticleLink(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <input className = "border" type="submit" value="Submit" />
       </div>
     </form>
   )
